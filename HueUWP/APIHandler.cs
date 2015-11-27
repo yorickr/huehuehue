badecbeac307d9135f14c6e42874f456730c295d
Yorick Rommers <yorick-rommers@hotmail.com>
2015-11-27 12:35:52 +0100

    Added APIHandler

---
+++ HueUWP/APIHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace HueUWP
+{
+    class APIHandler
+    {
+        public static ApplicationData APP_DATA = ApplicationData.Current;
+        public static ApplicationDataContainer LOCAL_SETTINGS = APP_DATA.LocalSettings;
+
+        NetworkHandler nwh;
+        public APIHandler(NetworkHandler nwh)
+        {
+            this.nwh = nwh;
+        }
+
+        public void Register()
+        {
+            string id = nwh.RegisterName("Hue", "Kenneth&Yorick").ToString();
+            //write to file;
+        }
+    }
+}
