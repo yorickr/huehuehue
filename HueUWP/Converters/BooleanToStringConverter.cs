a098c1738cafe99403da7f573b59c3ca0aa5043c
Kenneth van Ewijk <kennyboy@live.nl>
2015-12-01 13:18:00 +0100

    Added converter and more pages

---
+++ HueUWP/Converters/BooleanToStringConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace HueUWP.Converters
+{
+    public class BooleanToStringConverter : IValueConverter
+    {
+        //From bool to nullable
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool b = (bool)value;
+            return b.ToString();
+        }
+
+        //From nullable to bool
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            string s = (string)value;
+            bool b = false;
+
+            if (s == "true")
+                b = true;
+
+            return b;
+        }
+    }
+}
