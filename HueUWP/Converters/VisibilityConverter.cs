0e0edae19caea6c30ac50ea40c8f3f3617bfd42d
Kenneth van Ewijk <kennyboy@live.nl>
2015-12-03 11:34:21 +0100

    Added new converter

---
+++ HueUWP/Converters/VisibilityConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace HueUWP.Converters
+{
+    class VisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool b = (bool)value;
+
+            if (b)
+                return Visibility.Visible;
+            else
+                return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            Visibility v = (Visibility)value;
+
+            if (v == Visibility.Visible)
+                return true;
+            else
+                return false;
+        }
+    }
+}
